87555a43255143f622ccbd18a06cce5207ef856e
Danielle Strazzeri <dstrazzeri303@gmail.com"git config --global user.name "Danielle Strazzeri>
2021-09-17 14:50:17 -0400

    some progress has been made

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 /* 🚀🚀🚀🤓 Task 1: 🤓🚀🚀🚀 
 Study the code below and explain in your own words why nested function can access the variable internal. */
 
+const { numberLiteralTypeAnnotation } = require("@babel/types");
+
 const external = "I'm outside the function";
 
 function myFunction() {
@@ -28,14 +30,15 @@ myFunction();
     
 For example, `summation(4)` should return 10 because 1+2+3+4 is 10. Note, you may use a for loop for this function if you wish */
 
-function summation(n) {
-  let sum = 0;
-  for (let i = 1; i <= n; i++){
-    sum += i;
- 
- return sum; }
+function summation(num) {
+  let count = 0;
+  for(let i = 0; i < num; i++){
+    count = count + num();
+  }
+  return num;
 }
-summation(4);
+
+
 // 🦁🦁🦁 Topic 2: ADVANCED Array Methods 🦁🦁🦁
 // Given this zoo data from around the United States, follow the instructions below. Use the specific array methods in the requests below to solve the problems.
 
@@ -132,9 +135,10 @@ function multiply(a, b){
  // 🦁🦁🦁 Use greeting to accept a first and last name and return "Hello {first-name} {last-name}, nice to meet you!" 🦁🦁🦁
   
 function greeting(first, last){
-   return `Hello {first-name} {last-name}, nice to meet you!`;
+   return (`Hello ${first} ${last}, nice to meet you!`);
   }
-  
+ console.log(greeting('Jane', 'Doe'))
+
   // 🦁🦁🦁 Step 3: Check your work by un-commenting the following calls to consume(): 🦁🦁🦁 
   // ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️
   // console.log(consume(2, 2, add)); // 4
@@ -146,8 +150,11 @@ function greeting(first, last){
 /* 🐴🐴🐴 Step 1: Base Constructor 🐴🐴🐴
  Use the constructor function named CuboidMaker to accept properties for length, width, and height which can be initialized as an object
 */
-function CuboidMaker(/*Your Code Here */){
-  /*Your Code Here */
+
+function CuboidMaker(attr){
+ this.length = attr.length;
+ this.width = attr.width;
+ this.height = attr.height;
 }
 
 
@@ -155,7 +162,9 @@ function CuboidMaker(/*Your Code Here */){
   Create a method called volume using CuboidMaker's prototype that returns the volume of a given cuboid's length, width, and height
   Formula for cuboid volume: length * width * height   */
 
-
+CuboidMaker.prototype.volume = function(){
+return this.length * this.width * this.height;
+}
 
 
 
@@ -164,7 +173,9 @@ function CuboidMaker(/*Your Code Here */){
   Formula for cuboid surface area of a cube: 
   2 * (length * width + length * height + width * height)  */
 
-
+CuboidMaker.prototype.surfaceArea = function(){
+  return this.surfaceArea = 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+}
 
 
 
@@ -172,26 +183,36 @@ function CuboidMaker(/*Your Code Here */){
   Create an object called cuboid that uses the new keyword to use our CuboidMaker constructor
   Add properties and values of length: 4, width: 5, and height: 5 to cuboid. */
 
-
+const cuboid = new CuboidMaker(4, 5, 5);
 
 
 
 // 🐴🐴🐴 Test your volume and surfaceArea methods by uncommenting the logs below: 🐴🐴🐴
 // ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+// console.log('topic3', cuboid.volume()); // 100
+// console.log('topic3', cuboid.surfaceArea()); // 130
  
 
 // 🦄🦄🦄 Topic 4: Classes 🦄🦄🦄 //
 //🦄🦄🦄 1. Take your prototypes from above and refactor into class syntax. Please rename your class CuboidMakerTwo and your object cuboidTwo 🦄🦄🦄
 class CuboidMakerTwo{
-
+constructor(attr){
+  this.length = attr.length;
+  this.width = attr.width;
+  this.height = attr.height;
 }
-
+volume(){
+  return this.volume = this.length * this.width * this.height;
+}
+surfaceArea(){
+return this.surfaceArea = 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+}
+}
+const cuboidTwo = new CuboidMakerTwo(4, 5, 5);
 
 //🦄🦄🦄 Test your volume and surfaceArea methods by uncommenting the logs below: 🦄🦄🦄
-// console.log(cuboidTwo.volume()); // 100
-// console.log(cuboidTwo.surfaceArea()); // 130
+// console.log('topic4', cuboidTwo.volume()); // 100
+// console.log('topic4', cuboidTwo.surfaceArea()); // 130
 
 
 
